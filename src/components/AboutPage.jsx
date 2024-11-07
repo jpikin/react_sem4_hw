146782d024b76bc1d772af61c1f8f13f0441e1d7
pikin <pikin@bk.ru>
2024-11-08 00:46:38 +0500

    css fix

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -3,8 +3,10 @@ import { Link } from 'react-router-dom';
 function AboutPage() {
     return (
         <>
+        
             <h1>О нас</h1>
             <Link to='/'>Перейти на страницу Главная</Link>
+            
         </>
     );
 }
--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -1,22 +1,26 @@
 import '../notFoundPage.css'
+import { Link } from 'react-router-dom';
 
 function NotFoundPage() {
     return (
-      <>
+        <>
+            <Link
+                to='/'
+                className="goTo"
+            >На главную</Link>
+            <h1 className='h1'>
+                404
+                <span aria-hidden="true">404</span>
+            </h1>
+            <div className="cloak__wrapper">
+                <div className="cloak__container">
+                    <div className="cloak"></div>
+                </div>
+            </div>
 
-        <h1 className='h1'>
-          404
-          <span aria-hidden="true">404</span>
-        </h1>
-        <div className="cloak__wrapper">
-          <div className="cloak__container">
-            <div className="cloak"></div>
-          </div>
-        </div>
-        <div className="info">          
-        </div>
-      </>
+
+        </>
     );
-  }
-  
-  export default NotFoundPage;
+}
+
+export default NotFoundPage;
