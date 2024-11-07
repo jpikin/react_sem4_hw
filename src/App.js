2525f98a6cf44e4c5f07d3f02cf29c1e9dd58548
pikin <pikin@bk.ru>
2024-11-08 00:16:15 +0500

    404 added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
+import NotFoundPage from './components/NotFoundPage';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/about' element={<AboutPage />} />
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </Router>
   );
---
+++ src/components/NotFoundPage.jsx
@@ -0,0 +1,22 @@
+import '../notFoundPage.css'
+
+function NotFoundPage() {
+    return (
+      <>
+
+        <h1>
+          404
+          <span aria-hidden="true">404</span>
+        </h1>
+        <div className="cloak__wrapper">
+          <div className="cloak__container">
+            <div className="cloak"></div>
+          </div>
+        </div>
+        <div className="info">          
+        </div>
+      </>
+    );
+  }
+  
+  export default NotFoundPage;
